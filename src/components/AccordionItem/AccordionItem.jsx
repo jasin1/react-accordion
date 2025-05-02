98e07ed1c8a4061d5d6747be48c2cf22a2e1ba2f
jasin1 <jasin.tairaidrissi@gmail.com>
2025-05-02 15:04:22 +0200

    create first component

---
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -0,0 +1,16 @@
+import './AccordionItem.css';
+// import { useState } from 'react';
+
+function AccordionItem(){
+  // const [selected, setSelected]=useState(false);
+
+  // function handleSelect(){
+  //   setSelected(true);
+  // }
+
+  return(
+    <h3 className='Acc-title'>Test</h3>
+  )
+}
+
+export default AccordionItem;
