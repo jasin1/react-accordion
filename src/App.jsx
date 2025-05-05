74da23ecc442acf97cd95d3de7aecade63c5580c
jasin1 <jasin.tairaidrissi@gmail.com>
2025-05-05 12:15:39 +0200

    Add acoordion logic

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,16 @@ function App() {
     <>
     <main>
       <h1>Accordion</h1>
-      <AccordionItem/>
+      <AccordionItem title={'Getting Started'}>
+        <p>
+        Mauris ut iaculis ante. Ut non neque sit amet leo malesuada convallis. Vestibulum at mattis nibh. Donec at neque sapien. Fusce posuere auctor mauris ac vehicula. Nunc a dolor at arcu iaculis ultricies. Nullam pharetra lectus viverra odio rhoncus posuere. Pellentesque luctus gravida venenatis. Praesent blandit ligula sit amet elit auctor blandit. Donec porta erat eget efficitur convallis. In congue, nisl non tempor vestibulum, ipsum lectus euismod urna, ac laoreet massa nisi eget dolor. Praesent sollicitudin augue arcu, id semper risus viverra at. Nulla rhoncus augue est, nec fringilla est tincidunt venenatis.
+        </p>
+      </AccordionItem>
+      <AccordionItem title={'Billing'}>
+        <p>
+        Mauris ut iaculis ante. Ut non neque sit amet leo malesuada convallis. Vestibulum at mattis nibh. Donec at neque sapien. Fusce posuere auctor mauris ac vehicula. Nunc a dolor at arcu iaculis ultricies. Nullam pharetra lectus viverra odio rhoncus posuere. Pellentesque luctus gravida venenatis. Praesent blandit ligula sit amet elit auctor blandit. Donec porta erat eget efficitur convallis. In congue, nisl non tempor vestibulum, ipsum lectus euismod urna, ac laoreet massa nisi eget dolor. Praesent sollicitudin augue arcu, id semper risus viverra at. Nulla rhoncus augue est, nec fringilla est tincidunt venenatis.
+        </p>
+      </AccordionItem>
 
  
 
--- src/components/AccordionItem/AccordionItem.jsx
+++ src/components/AccordionItem/AccordionItem.jsx
@@ -1,16 +1,32 @@
-import './AccordionItem.css';
-// import { useState } from 'react';
+// import { Children } from "react";
+import "./AccordionItem.css";
+import Arrow from "../../assets/arrow.svg";
+import { useRef, useState } from "react";
 
-function AccordionItem(){
-  // const [selected, setSelected]=useState(false);
+function AccordionItem({ title, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentRef = useRef(null);
 
-  // function handleSelect(){
-  //   setSelected(true);
-  // }
+  function toggleAccordion() {
+    setIsOpen(!isOpen);
+  }
 
-  return(
-    <h3 className='Acc-title'>Test</h3>
-  )
+  return (
+    <div className="Acc-wrapper">
+      <div className="Acc-header" onClick={toggleAccordion}>
+        <h3 className="Acc-title">{title}</h3>
+        <img className={`Icon-arrow ${isOpen ? "rotate" : ""}`} src={Arrow} />
+      </div>
+      <div ref={contentRef} 
+      className="Acc-content"
+      style={{
+        maxHeight: isOpen ? `${contentRef.current?.scrollHeight}px`: '0px'
+      }}
+      >
+        <div className="Acc-content-inner">{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default AccordionItem;
