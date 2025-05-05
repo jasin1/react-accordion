c13bab86edc0a21e8052fb3914a0e8f49ba648cd
jasin1 <jasin.tairaidrissi@gmail.com>
2025-05-05 12:55:01 +0200

    accordion max width

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ function App() {
       <AccordionItem title={'Billing'}>
         <p>
         Mauris ut iaculis ante. Ut non neque sit amet leo malesuada convallis. Vestibulum at mattis nibh. Donec at neque sapien. Fusce posuere auctor mauris ac vehicula. Nunc a dolor at arcu iaculis ultricies. Nullam pharetra lectus viverra odio rhoncus posuere. Pellentesque luctus gravida venenatis. Praesent blandit ligula sit amet elit auctor blandit. Donec porta erat eget efficitur convallis. In congue, nisl non tempor vestibulum, ipsum lectus euismod urna, ac laoreet massa nisi eget dolor. Praesent sollicitudin augue arcu, id semper risus viverra at. Nulla rhoncus augue est, nec fringilla est tincidunt venenatis.
+        Mauris ut iaculis ante. Ut non neque sit amet leo malesuada convallis. Vestibulum at mattis nibh. Donec at neque sapien. Fusce posuere auctor mauris ac vehicula. Nunc a dolor at arcu iaculis ultricies. Nullam pharetra lectus viverra odio rhoncus posuere. Pellentesque luctus gravida venenatis. Praesent blandit ligula sit amet elit auctor blandit. Donec porta erat eget efficitur convallis. In congue, nisl non tempor vestibulum, ipsum lectus euismod urna, ac laoreet massa nisi eget dolor. Praesent sollicitudin augue arcu, id semper risus viverra at. Nulla rhoncus augue est, nec fringilla est tincidunt venenatis.
         </p>
       </AccordionItem>
 
